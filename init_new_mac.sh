8efc5770ada80d672fef99f6c46981eec9d2c46b
yuhao <liaoyuhao@gengmei.com>
2017-12-03 14:58:19 +0800

    first commit

---
+++ init_new_mac.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install tree
+brew install wget
+brew install httpie
+
